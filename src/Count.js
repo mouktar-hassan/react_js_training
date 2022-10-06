8f69ea28fe874b51616187301bc0dd43aa37e439
mouktar-hassan <43705682+mouktar-hassan@users.noreply.github.com>
2022-10-06 17:19:00 +0200

    Count.js file modified, class component and functional component to increment and decrement a value

--- src/Count.js
+++ src/Count.js
@@ -1,34 +1,25 @@
-import React, {Component} from "react";
-
+import React, { Component, useState, useEffect } from 'react';
+//class component
 class Count extends Component{
   constructor(props){
     super(props);
-    increment : 0;
     this.state = {
       counter:0
-    } 
+    }
+
+    this.increment = this.increment.bind(this)
 
   }
 
-  // componentDidMount(){
+increment = () => {
+  this.setState({ counter: this.state.counter + 1 });
+};
 
-  // }
-
-  // componentWillUnMount(){
-
-  // }
-
-  increment = () => {
-    // this.setState((state,props)=>({
-    //   counter : state.counter + props.increment
-    // }));
-    this.setState({ counter : this.state.counter + 1 });
-  }
   decrement = () => {
-    (this.state.counter >=1) 
-    ? 
+    (this.state.counter >=1)
+    ?
     this.setState({ counter : this.state.counter - 1 })
-    : 
+    :
     this.setState({ counter : 0 })
   }
 
@@ -38,9 +29,36 @@ class Count extends Component{
         <div>{this.state.counter}</div>
         <button onClick={this.increment} >Increment</button>
         <button onClick={this.decrement} >Decrement</button>
-      </div>      
+      </div>
     )
   }
 }
+
+//functional component
+// function Count(){
+
+//   const [counter, setCounter] = useState(0);
+
+//   function increment(){
+//     setCounter(counter+1);
+//   }
+
+//   function decrement(){
+//     (counter >= 1)
+//     ?
+//     (setCounter(counter-1))
+//     :
+//     (setCounter(0))
+//   }
+
+//   return <div>
+//     {counter}
+//     <div>
+//       <button onClick ={increment} >Increment</button>
+//       <button onClick ={decrement} >Decrement</button>
+//     </div>
+//   </div>;
+
+// }
 
 export default Count;
